74c94ef0de6fb648b471fa868531058a2d013802
Yibdo <scbramley7@gmail.com>
2025-10-10 19:31:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+  const title = document.querySelector('.dropdown-title');
+  const content = document.querySelector('.dropdown-content');
+
+  title.addEventListener('click', () => {
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+  });
+
+
